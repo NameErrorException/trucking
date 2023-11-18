0f3bddeac763bda96aa0781df808ddf4008787aa
eversane <30333175+Eversane@users.noreply.github.com>
2023-11-18 14:15:59 -0500

    --

---
+++ change_email_adrress.sh
@@ -0,0 +1,16 @@
+git filter-branch --env-filter '
+OLD_EMAIL="dustboy18@gmail.com"
+CORRECT_NAME="__"
+CORRECT_EMAIL="30333175+Eversane@users.noreply.github.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
