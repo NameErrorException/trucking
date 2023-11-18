c6752d1b735329092e1d0e16a6be93dcccb40a30
Bohan <78827836+s026352@users.noreply.github.com>
2023-11-18 18:07:06 -0500

    add map markers, mapp styling, map routes,

--- fe/src/pages/Home.jsx
+++ fe/src/pages/Home.jsx
@@ -11,37 +11,12 @@ export default function Home() {
   navigate(path)
   }
   //fatch data
-  const [users, setUsers] = useState([])
 
-  const fetchUserData = () => {
-    fetch("http://127.0.0.1:5000/data")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setUsers(data)
-      })
-  }
-
-  useEffect(() => {
-    fetchUserData()
-    
-  }, [])
 
   return (
     
-    <>
-    <div>
-      {users.length > 0 && (
-        <ul> 
-          {users.map(user => (
-            <li key={user.id}> {console.log(users)}{user.name}</li>
-          ))}
-        </ul>
-      )}
-    </div>
-     
-    <div className="text-gray-400 bg-gray-900 body-font" >
+    <>    
+    <div className="text-gray-400 bg-slate-500 body-font" >
         
     <header>
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -56,13 +31,13 @@ export default function Home() {
     </nav>
     </div>
     </header>
-    <div className='ml-10 h-screen w-screen bg-gray-900'>
+    <div className='ml-10 mr-0 h-screen w-screen bg-gray-900'>
       <MapComponent />
 
     </div>
     <div className="flex flex-wrap">
         {/* Sidebar */}
-        <aside className=" mt-16 text-gray-400 bg-gray-900 body-font w-1/5 overflow-y-auto h-screen fixed top-0 right-10">
+        <aside className=" mt-16 text-gray-400 bg-transparent body-font w-1/5 overflow-y-auto h-screen fixed top-0 right-10">
           <div className="container mx-auto flex flex-col items-center p-5">
             <a className="flex title-font font-medium items-center text-white mb-4">
               <span className="ml-3 text-lg hover:text-white shadow-lg transition duration-300 ease-in-out transform hover:scale-105">Truck Info</span>
