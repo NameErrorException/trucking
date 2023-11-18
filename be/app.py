70adce0b485c4849807b483d2d1655cb8e2d4df7
q <30962319+lectern@users.noreply.github.com>
2023-11-17 23:53:40 -0500

    add get stream data + endpoints w fake data for testing.

--- be/app.py
+++ be/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 
 from safety.camera import Camera
+from jobs.data import Data
 
 app = Flask(__name__)
 camera = Camera()
+data = Data()
 
 # NOTE:
 # https://www.digitalocean.com/community/tutorials/how-to-create-your-first-web-application-using-flask-and-python-3
@@ -15,6 +17,15 @@ def hello():
 @app.route('/camera')
 def get_camera():
     return Response(camera.get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/data')
+def get_current_data():
+    return jsonify(data.get_current_data())
+
+@app.route('/filtered')
+def get_job_data():
+    # the notification job list
+    return jsonify(data.get_filtered())
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
---
+++ be/jobs/data.py
@@ -0,0 +1,72 @@
+import paho.mqtt.client as mqtt
+
+class Data:
+    def __init__(self):
+        self.client = mqtt.Client(client_id="NNE02", clean_session=True)
+        self.client.username_pw_set("CodeJamUser", "123CodeJam")
+
+        # Assign callback functions
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        self.client.subscribe("CodeJam", qos=1)
+
+    def on_message(self, client, userdata, msg):
+        print(f"{msg.payload.decode()}")
+
+        # manipulate data here
+
+    def get_data_stream(self):
+        self.client.connect("fortuitous-welder.cloudmqtt.com", 1883, 60)
+        self.client.loop_forever()
+
+    def get_current_data(self):
+        temp_data = [
+            {"seq":1935,"type":"Truck","timestamp":"2023-11-17T18:43:37","truckId":358,"positionLatitude":36.94601821899414,"positionLongitude":-81.04188537597656,"equipType":"Flatbed","nextTripLengthPreference":"Short"},
+            {"seq":1926,"type":"Truck","timestamp":"2023-11-17T18:42:46","truckId":268,"positionLatitude":39.38967514038086,"positionLongitude":-84.36373138427734,"equipType":"Van","nextTripLengthPreference":"Short"},
+
+            {"seq":1921,"type":"Load","timestamp":"2023-11-17T18:41:19","loadId":10408,"originLatitude":42.7893,"originLongitude":-73.9809,"destinationLatitude":41.7761,"destinationLongitude":-72.5213,"equipmentType":"Van","price":497.0,"mileage":129.0},
+            {"seq":131,"type":"Load","timestamp":"2023-11-17T09:31:59","loadId":10987,"originLatitude":39.19,"originLongitude":-76.72,"destinationLatitude":41.29,"destinationLongitude":-81.48,"equipmentType":"Van","price":650.0,"mileage":350.0}
+        ]
+        return temp_data
+
+    def get_filtered(self):
+        """
+        return the trucks and their notificatons.
+        format:
+        'truckID': {
+            [list of loads]
+        }
+
+        notification:
+            - return load data without seq and type
+            - TODO: add return type
+        ...
+        """
+        return [
+            [
+                {
+                    "truckId": 358,
+                    "notifications": [
+                        {"timestamp":"2023-11-17T18:41:19","loadId":10408,"originLatitude":42.7893,"originLongitude":-73.9809,"destinationLatitude":41.7761,"destinationLongitude":-72.5213,"equipmentType":"Van","price":497.0,"mileage":129.0},
+                        {"timestamp":"2023-11-17T09:31:59","loadId":10987,"originLatitude":39.19,"originLongitude":-76.72,"destinationLatitude":41.29,"destinationLongitude":-81.48,"equipmentType":"Van","price":650.0,"mileage":350.0}
+                    ]
+                },
+                {
+                    "truckId": 268,
+                    "notifications": [
+                        {"timestamp":"2023-11-17T18:41:19","loadId":10408,"originLatitude":42.7893,"originLongitude":-73.9809,"destinationLatitude":41.7761,"destinationLongitude":-72.5213,"equipmentType":"Van","price":497.0,"mileage":129.0},
+                        {"timestamp":"2023-11-17T09:31:59","loadId":10987,"originLatitude":39.19,"originLongitude":-76.72,"destinationLatitude":41.29,"destinationLongitude":-81.48,"equipmentType":"Van","price":650.0,"mileage":350.0}
+                    ]
+                } 
+
+            ]
+        ]
+
+
+
+if __name__ == "__main__":
+    data = Data()
+    data.get_data_stream()
